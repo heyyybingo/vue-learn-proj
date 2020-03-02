ae6c59c772f5a40c4b06b5790752131e74a93922
heyyybingo <283385508@qq.com>
2020-03-02 23:07:22 +0800

    增加了首页的轮播动画，以及九宫格图标

--- src/components/HomeContainer.vue
+++ src/components/HomeContainer.vue
@@ -1,6 +1,35 @@
 <template>
   <div>
-    <h3>Home</h3>
+    <mt-swipe :auto="10000">
+      <mt-swipe-item v-for="item in lunbotulist" :key="item">
+        <img :src="item" width="100%" height="100%" />
+      </mt-swipe-item>
+    </mt-swipe>
+
+    <div class="blocks">
+      <div class="blocks-row">
+        <div>
+          <span class="iconfont icon-dingwei"></span>
+        </div>
+        <div>
+          <span class="iconfont icon-pengyouquan"></span>
+        </div>
+        <div>
+          <span class="iconfont icon-lihe"></span>
+        </div>
+      </div>
+      <div class="blocks-row">
+        <div>
+          <span class="iconfont icon-jiju"></span>
+        </div>
+        <div>
+          <span class="iconfont icon-xiaoxizhongxin"></span>
+        </div>
+        <div>
+          <span class="iconfont icon-bangzhu"></span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,7 +37,14 @@
 export default {
   name: "",
   data() {
-    return {};
+    return {
+      lunbotulist: [
+        "./src/lib/image/lunbotu/1.jpg",
+        "./src/lib/image/lunbotu/2.jpg",
+        "./src/lib/image/lunbotu/3.jpg",
+        "./src/lib/image/lunbotu/4.jpg"
+      ]
+    };
   },
   components: {},
   methods: {}
@@ -16,4 +52,29 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.mint-swipe {
+  height: 200px;
+}
+.blocks {
+  margin-top: 10%;
+  .blocks-row {
+    width: 100%;
+    display: flex;
+    height: 50px;
+    div {
+      margin-left: 5%;
+      margin-right: 5%;
+      height: 50%;
+      flex-grow: 1;
+      display: flex;
+      align-items: center;
+      border-radius: 50;
+
+      span {
+        text-align: center;
+        flex-grow: 1;
+      }
+    }
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -5,12 +5,18 @@ import router from './router.js'
 import {
     Header,
     Tabbar,
-    TabItem
+    TabItem,
+    Swipe,
+    SwipeItem
 } from 'mint-ui'
+
+import './lib/icon/font_q4u7hjhkkkh/iconfont.css'
 import './lib/icon/font_2cnnszcve7q/iconfont.css'
 Vue.component(Header.name, Header);
 Vue.component(Tabbar.name, Tabbar);
 Vue.component(TabItem.name, TabItem);
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
 
 var vm = new Vue({
     router,
